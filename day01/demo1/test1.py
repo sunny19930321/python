7381da885374e2de96dbb36aa5224876b23c735b
liyang@yodo1.com <liyang@yodo1.com>
2017-11-08 17:39:08 +0800

    python基础知识1

--- day01/demo1/test1.py
+++ day01/demo1/test1.py
@@ -1,3 +1,21 @@
 import stand
-
+import sys
 print stand.new_str
+
+d = [1,2,3]
+
+count = sys.getrefcount(d)
+print count
+a = 1
+b = 1
+c = 2
+d = 4
+if (a == b) and (c == d):
+    print "ok"
+else:
+    print "no"
+    # del a,b,c,d
+
+str1 = "abc"
+str2 = str1.lower()
+print str2
